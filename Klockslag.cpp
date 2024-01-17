1419170c2f7419fc959c1db164b70e3b0c66aa1f
creeperdoglas <borenmelkan@gmail.com>
2024-01-17 11:50:02 +0100

    beginning

--- Klockslag.cpp
+++ Klockslag.cpp
@@ -1,5 +1,8 @@
+#include <iostream>
+#include <fstream>
+#include "klockslag.h"
 
-
+using namespace std;
 int main(){
 
 };
---
+++ klockslag.h
@@ -0,0 +1,10 @@
+#pragma once
+#include <fstream>
+#include <sstream>
+#include <vector>
+class Time
+{
+public:
+private:
+  int hour, minute, second;
+};

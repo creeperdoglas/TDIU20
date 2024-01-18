3a15550e3b4edd217dad0df5024bfc8d8a26034d
Melker Gustafsson <borenmelkan@gmail.com>
2024-01-18 15:31:26 +0100

    GettingSomewhere

--- time_test.cc
+++ time_test.cc
@@ -5,59 +5,59 @@ using namespace std;
 
 TEST_CASE("Constructors and getters")
 {
-  SECTION("Default")
-  {
-    Time empty{};
-    CHECK(empty.get_hour() == 0);
-    CHECK(empty.get_minute() == 0);
-    CHECK(empty.get_second() == 0);
-  }
-  /* ----- REMOVE THIS COMMENT WHEN PREVIOUS TEST PASSES -----
+   SECTION("Default")
+   {
+      Time empty{};
+      CHECK(empty.get_hour() == 0);
+      CHECK(empty.get_minute() == 0);
+      CHECK(empty.get_second() == 0);
+   }
+   /* ----- REMOVE THIS COMMENT WHEN PREVIOUS TEST PASSES -----
 
-     SECTION("Integer")
-     {
-        Time t0{0,0,0};
-        Time t1{12,30,30};
-        Time t2{23,59,59};
+      SECTION("Integer")
+      {
+         Time t0{0,0,0};
+         Time t1{12,30,30};
+         Time t2{23,59,59};
 
-        CHECK_THROWS( Time{13,35,60} );
-        CHECK_THROWS( Time{13,60,35} );
-        CHECK_THROWS( Time{24,35,35} );
+         CHECK_THROWS( Time{13,35,60} );
+         CHECK_THROWS( Time{13,60,35} );
+         CHECK_THROWS( Time{24,35,35} );
 
-        CHECK( t0.get_hour()   == 0 );
-        CHECK( t0.get_minute() == 0 );
-        CHECK( t0.get_second() == 0 );
-        CHECK( t1.get_hour()   == 12 );
-        CHECK( t1.get_minute() == 30 );
-        CHECK( t1.get_second() == 30 );
-        CHECK( t2.get_hour()   == 23 );
-        CHECK( t2.get_minute() == 59 );
-        CHECK( t2.get_second() == 59 );
-     }
-  */
-  /* ----- REMOVE THIS COMMENT WHEN PREVIOUS TEST PASSES -----
+         CHECK( t0.get_hour()   == 0 );
+         CHECK( t0.get_minute() == 0 );
+         CHECK( t0.get_second() == 0 );
+         CHECK( t1.get_hour()   == 12 );
+         CHECK( t1.get_minute() == 30 );
+         CHECK( t1.get_second() == 30 );
+         CHECK( t2.get_hour()   == 23 );
+         CHECK( t2.get_minute() == 59 );
+         CHECK( t2.get_second() == 59 );
+      }
+   */
+   /* ----- REMOVE THIS COMMENT WHEN PREVIOUS TEST PASSES -----
 
-     SECTION("String")
-     {
-        Time t0{"00:00:00"};
-        Time t1{"12:30:30"};
-        Time t2{"23:59:59"};
+      SECTION("String")
+      {
+         Time t0{"00:00:00"};
+         Time t1{"12:30:30"};
+         Time t2{"23:59:59"};
 
-        CHECK_THROWS( Time{"13:35:60"} );
-        CHECK_THROWS( Time{"13:60:35"} );
-        CHECK_THROWS( Time{"24:35:35"} );
+         CHECK_THROWS( Time{"13:35:60"} );
+         CHECK_THROWS( Time{"13:60:35"} );
+         CHECK_THROWS( Time{"24:35:35"} );
 
-        CHECK( t0.get_hour()   == 0 );
-        CHECK( t0.get_minute() == 0 );
-        CHECK( t0.get_second() == 0 );
-        CHECK( t1.get_hour()   == 12 );
-        CHECK( t1.get_minute() == 30 );
-        CHECK( t1.get_second() == 30 );
-        CHECK( t2.get_hour()   == 23 );
-        CHECK( t2.get_minute() == 59 );
-        CHECK( t2.get_second() == 59 );
-     }
-  */
+         CHECK( t0.get_hour()   == 0 );
+         CHECK( t0.get_minute() == 0 );
+         CHECK( t0.get_second() == 0 );
+         CHECK( t1.get_hour()   == 12 );
+         CHECK( t1.get_minute() == 30 );
+         CHECK( t1.get_second() == 30 );
+         CHECK( t2.get_hour()   == 23 );
+         CHECK( t2.get_minute() == 59 );
+         CHECK( t2.get_second() == 59 );
+      }
+   */
 }
 /* ----- REMOVE THIS COMMENT WHEN PREVIOUS TEST PASSES -----
 TEST_CASE ("is_am")

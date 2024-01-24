342bdce29170a3ddd808a8e2ded565b94d567fd2
Melker Gustafsson <borenmelkan@gmail.com>
2024-01-24 16:25:13 +0100

    MyGeniusGeneratesGravity

--- Time.cc
+++ Time.cc
@@ -1,7 +1,8 @@
 #include <string>
 #include <iostream>
 #include <fstream>
-#include <stdexcept>
+#include <stdexcept> //för throw invalid_argument
+#include <iomanip>   // för std::setw and std::setfill
 #include <sstream>
 #include "Time.h"
 
@@ -45,7 +46,7 @@ bool check_for_invalid_input(int const h, int const m, int const s)
   }
 }
 
-bool Time::is_am()
+bool Time::is_am() const
 {
   if (hour < 12)
   {
@@ -55,4 +56,269 @@ bool Time::is_am()
   {
     return false;
   }
+}
+
+string Time::to_string(const bool am_pm_format) const
+{
+  ostringstream ss{};
+
+  int display_hour = hour;
+  if (am_pm_format)
+  {
+    if (hour == 0)
+    {
+      display_hour = 12; // 12 AM
+    }
+    else if (hour > 12)
+    {
+      display_hour = hour - 12; // konvertera till 12-timmars format
+    }
+  }
+
+  ss << setw(2) << setfill('0') << display_hour;
+  ss << ':' << setw(2) << setfill('0') << minute;
+  ss << ':' << setw(2) << setfill('0') << second;
+
+  if (am_pm_format)
+  {
+    ss << (is_am() ? " AM" : " PM");
+  }
+
+  return ss.str();
+}
+// gör så att om n < 10 lägg till 0a.
+void Time::format_into_ostream(int const n, ostream &os) const
+{
+  if (n < 10)
+  {
+    os << 0;
+  }
+  os << n;
+}
+
+// gammal operator+ , kom fram till att den tar överdrivet lång tid vid stora tal. Den nya använder inte en loop och borde (är) snabbare :D
+//  Time Time::operator+(int const n) const
+//  {
+//    Time temp{hour, minute, second};
+//    if (n < 0)
+//    {
+//      temp = temp - (-n);
+//    }
+//    else
+//    {
+
+//      for (int i{}; i < n; i++)
+//      {
+//        temp++;
+//    }
+//   }
+//   return temp;
+// }
+
+Time Time::operator+(int const n) const
+{
+  // Konvertera tid till totala sekunder
+  int total_seconds = hour * 3600 + minute * 60 + second;
+
+  // Lägg till n sekunder
+  total_seconds += n;
+
+  // för överflöd
+  total_seconds = (total_seconds + 24 * 3600) % (24 * 3600);
+
+  // Konvertera tillbaka till timmar, minuter och sekunder
+  int new_hour = total_seconds / 3600;
+  int new_minute = (total_seconds % 3600) / 60;
+  int new_second = total_seconds % 60;
+
+  // Skapa och returnera
+  return Time(new_hour, new_minute, new_second);
+}
+Time Time::operator++(int)
+{
+  // skapar en kopia och returnerar
+  Time t{*this};
+  ++(*this); // lade till prefix i h fil, nu funkar det.
+  return t;
+}
+Time &Time::operator++()
+{
+  // skulle vilja använda else if osv, men tror inte alla fall funkar då tex 02:59:59? , skulle bli 02:60:00 tror jag
+  second++;
+  if (second > 59)
+  {
+    second = 0;
+    minute++;
+    if (minute > 59)
+    {
+      minute = 0;
+      hour++;
+      if (hour > 23)
+      {
+        hour = 0;
+      }
+    }
+  }
+  return *this;
+}
+
+Time Time::operator-(int const n) const
+{
+  // Konvertera tid till totala sekunder
+  int total_seconds = hour * 3600 + minute * 60 + second;
+
+  // Subtrahera n sekunder
+  total_seconds -= n;
+
+  // för överflöd
+  total_seconds = (total_seconds + 24 * 3600) % (24 * 3600);
+
+  // Konvertera tillbaka till timmar, minuter och sekunder
+  int new_hour = total_seconds / 3600;
+  int new_minute = (total_seconds % 3600) / 60;
+  int new_second = total_seconds % 60;
+
+  // Skapa och returnera
+  return Time(new_hour, new_minute, new_second);
+}
+Time Time::operator--(int)
+{
+  Time t{*this};
+  --(*this);
+  return t;
+}
+Time &Time::operator--()
+{
+  second--;
+  if (second < 0)
+  {
+    second = 59;
+    minute--;
+    if (minute < 0)
+    {
+      minute = 59;
+      hour--;
+      if (hour < 0)
+      {
+        hour = 23;
+      }
+    }
+  }
+  return *this;
+}
+bool Time::operator==(Time const &t) const
+{
+  if (hour == t.hour && minute == t.minute && second == t.second)
+  {
+    return true;
+  }
+  else
+  {
+    return false;
+  }
+}
+
+bool Time::operator!=(Time const &t) const
+{
+  return !(*this == t);
+}
+
+bool Time::operator<(Time const &t) const
+{
+  if (hour < t.hour)
+  {
+    return true;
+  }
+  else
+  {
+    if (hour == t.hour && minute < t.minute)
+    {
+      return true;
+    }
+    else
+    {
+      if (hour == t.hour && minute == t.minute && second < t.second)
+      {
+        return true;
+      }
+      else
+      {
+        return false;
+      }
+    }
+  }
+}
+
+bool Time::operator>(Time const &t) const
+{
+  return (t < *this);
+}
+
+bool Time::operator<=(Time const &t) const
+{
+  if (*this < t || *this == t)
+  {
+    return true;
+  }
+  else
+  {
+    return false;
+  }
+}
+
+bool Time::operator>=(Time const &t) const
+{
+  if (*this > t || *this == t)
+  {
+    return true;
+  }
+  else
+  {
+    return false;
+  }
+}
+
+Time::operator string() const
+{
+  return to_string();
+}
+
+int Time::get_hour() const
+{
+  return hour;
+}
+
+int Time::get_minute() const
+{
+  return minute;
+}
+
+int Time::get_second() const
+{
+  return second;
+}
+
+ostream &operator<<(ostream &os, Time const &t)
+{
+  os << t.to_string();
+  return os;
+}
+istream &operator>>(istream &is, Time &t)
+{
+  int h, m, s;
+  char c1, c2;
+  is >> h >> c1 >> m >> c2 >> s;
+
+  // Skapa ett nytt objekt för validering och tilldelning, försökte använda en if sats förut med check_for_invalid input, funkade ej (antar då private?), så gjorde om såhär
+  try
+  {
+    Time temp(h, m, s); // Konstruktören kommer att kasta ett undantag om indata är ogiltig
+    t = temp;           // Uppdatera t om inga undantag kastas
+  }
+  catch (const std::invalid_argument &)
+  {
+    is.setstate(ios::failbit); // Ange felstatus vid ogiltig inmatning
+  }
+
+  return is;
 }
--- Time.h
+++ Time.h
@@ -1,7 +1,6 @@
 #pragma once // endast kompileras en gång
 #include <iostream>
-#include <sstream>
-#include <vector>
+#include <string>
 class Time
 {
 public:
@@ -13,16 +12,17 @@ public:
   int get_minute() const;
   int get_second() const;
 
-  bool is_am();
+  bool is_am() const;
   std::string to_string(const bool = false) const; // till att byta mellan 24 och 12 timmars
   operator std::string() const;                    // konvertera Time till sträng
 
   Time operator+(int const n) const; // Lägger till N sekunder
   Time operator++(int);              // post increment
-  // Time operator++(); kanske en till efter?, isåfall även på decrement med, testa sen.
+  Time &operator++();                // efter läst på lite så har jag kommit fram till att det här behövs, detta är en "prefix verision" , fortarande lite osäker men testat fram och funkar
 
   Time operator-(int const n) const; // tar bort sekunder
   Time operator--(int);              // post decrement
+  Time &operator--();
 
   // jämförelser
   bool operator==(Time const &t) const;
@@ -35,4 +35,8 @@ public:
 private:
   int hour{}, minute{}, second{};
   bool check_for_invalid_input(int const h, int const m, int const s);
+  void format_into_ostream(int const n, std::ostream &os) const;
 };
+// förstår inte riktigt varför man inte får använda friend (skulle då kunna lägga in klassen) Antar att det är dålig praxis då man inte vill att sådant i public ska komma åt sådant i private?
+std::ostream &operator<<(std::ostream &os, const Time &t);
+std::istream &operator>>(std::istream &is, Time &t);

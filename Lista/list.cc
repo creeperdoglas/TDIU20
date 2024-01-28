ed066dc00c9b83bb72928830c9594820c8a1ccef
Melker Gustafsson <borenmelkan@gmail.com>
2024-01-28 14:50:21 +0100

    BeginningUpg2

--- Lista/list.cc
+++ Lista/list.cc
@@ -1,1 +1,13 @@
-#pragma once
+#include "List.h"
+#include <stdexcept>
+#include <vector>
+
+using namespace std;
+
+List::List()
+{
+  last = new Element{};
+  first = new Element{};
+  last->prev = first;
+  first->next = last;
+}
--- Lista/list.h
+++ Lista/list.h
@@ -1,1 +1,16 @@
 #pragma once
+#include <iostream>
+#include <initializer_list>
+
+class List
+{
+private:
+  class Element
+  {
+  public:
+    Element() = default;
+    Element(int n);
+    ~Element();
+    Element
+  }
+};

cc17c582838d95799c61afcaeb6a486a425268e8
creeperdoglas <borenmelkan@gmail.com>
2024-01-31 10:28:17 +0100

    smallChange

--- Lista/list.h
+++ Lista/list.h
@@ -10,7 +10,10 @@ private:
   public:
     Element() = default;
     Element(int n);
-    ~Element();
-    Element
+    ~Element() = default;
+    Element(Element const &) = delete;
+    Element(Element &&) = delete;
+    Element operator=(Element const &) = delete;
+    Element operator=(Element &&) = delete;
   }
 };
